f8e95f1ed788e7252031968c84d86c20e1e658ef
fred onyango <dyfredieto71@gmail.com>
2019-03-04 11:43:09 +0300

    created variables on the scripts file

---
+++ JS/scripts.js
@@ -0,0 +1,4 @@
+var randomNumber = Math.floor(Math.random()*6)+1; //1-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+var randomNumber1 = Math.floor(Math.random()*6)+1; //1-6
+var playerOneImages1 = "images/dice"+randomNumber1+".png";
